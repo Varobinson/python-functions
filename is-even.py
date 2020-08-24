633c5dbbaa1200ecefc93bc4a37592c6809a8c82
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2020-08-24 12:04:51 -0400

    even and odd

---
+++ is-even.py
@@ -0,0 +1,15 @@
+# Initiating function
+def is_even(num):
+    #finding remainder if even print true
+    if num % 2 == 0:
+        return 'True'
+    else:  # if odd print false
+        return 'False'
+#handling value errors
+try:
+    user_input = int(input('Enter a number! '))
+except ValueError:
+    print('Must input a number')
+   
+answer = is_even(user_input)
+print(answer)
---
+++ is-odd.py
@@ -0,0 +1,27 @@
+# Initiating function
+def is_even(num):
+    #finding remainder if even print true
+    if num % 2 == 0:
+        return True
+    else:  # if odd print false
+        return False
+
+
+#handling value errors
+try:
+    user_input = int(input('Enter a number! '))
+except ValueError:
+    print('Must input a number')
+
+answer = is_even(user_input)
+print(answer)
+
+def is_odd():
+    if not is_even(user_input):
+        print('odd')
+    else:
+        print('even')
+
+is_odd()
+
+
